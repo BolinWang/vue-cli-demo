7d8b16ba10b469bfcd31c4c268b2130924ce1ec2
王玲波 <454949876@qq.com>
2019-01-10 13:27:47 +0800

    gh-pages

--- src/config-filter.js
+++
@@ -1,9 +0,0 @@
-import imageAli from '@/tools/image.js'
-
-function registerFilter (Vue) {
-  // 图片裁剪
-  Vue.filter('ali', imageAli)
-}
-
-export default registerFilter
-export { registerFilter }
--- src/mixins/pageShare.js
+++
@@ -1,9 +0,0 @@
-export default {
-  created () {
-    if (this.$route.path.indexOf('/distribution') === 0) {
-      this.wxSetShareSign({
-        hideShareMenu: true
-      })
-    }
-  }
-}
